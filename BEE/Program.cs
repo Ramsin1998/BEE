bd2960ac67f163738e1445f50e6dd36000af308e
Ramsin Shmayel <sanharib@chalmers.se>
2025-03-26 19:14:02 +0100

    Everything is in working order.

--- BEE/Program.cs
+++ BEE/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,9 +18,33 @@ namespace BEE
                 foreach (char c in Boolean.IdentifyVariables(text))
                     variables.Add(c, true);
 
-                Console.WriteLine(Boolean.Solve(text, variables));
+                Console.Clear();
 
-                //Boolean.isolateProducts(Console.ReadLine()).ToList().ForEach(Console.WriteLine);
+                foreach (var item in variables.Keys)
+                    Console.Write(item);
+
+                Console.WriteLine($"\t {text} \n");
+
+                for (int i=0; i < Math.Pow(2,variables.Count); i++)
+                {
+                    string bin = Convert.ToString(i, 2);
+                    bin = new string('0', variables.Count - bin.Length) + bin;
+
+                    for (int k = 0; k<bin.Length; k++)
+                    {
+                        variables[variables.ElementAt(k).Key] = bin[k] == '1';
+                    }
+
+                    bool result = Boolean.Solve(text, variables);
+
+                    Console.Write(bin + "\t");
+
+                    Console.ForegroundColor = result ? ConsoleColor.Cyan : ConsoleColor.Red;
+
+                    Console.WriteLine(result);
+
+                    Console.ResetColor();
+                }
 
                 Console.WriteLine();
             }
